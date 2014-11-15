3866af79b95d7b19478c0f68a721d17ec1fd49b3
Alexander Marchenko <maappdev@gmail.com>
2014-11-16 01:41:40 +0200

    change ip address

--- config/c9ide.js
+++ config/c9ide.js
@@ -1,6 +1,6 @@
 module.exports = {
 	script_path: '/home/naomay/cloud9/server.js',//'bin/c9ide/server.js',
-	ide_ip: '192.163.201.155',
+	ide_ip: '127.0.0.1',
 	ide_port: 3131,
 	workspace_path: '/home/naomay/git/project/' //temporary default path
 }
