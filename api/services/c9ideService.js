9365562ff04b7ac48f5a7fc1c35ba14aabf3734f
Alexander Marchenko <maappdev@gmail.com>
2014-11-15 21:55:49 +0200

    add debug running process

--- api/services/c9ideService.js
+++ api/services/c9ideService.js
@@ -9,11 +9,15 @@ function c9ideService(options){
 
 c9ideService.prototype = {
 	run: function(cb){
+		debug("Call run with options", this.options);
 		var c9ide = spawn(this.options.script_path, ['-l', this.options.ide_ip, '-p', this.options.ide_port, 
 			'-w', this.options.workspace_path]);
 		c9ide.stdout.on('data', function(data) {
 			cb(data);
 		});
+		c9ide.stderr.on('data', function(data) {
+			debug('Error',data);
+		};
 	}
 }
 module.exports = c9ideService;
