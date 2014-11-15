51056a6280ba5623ce509f8d4ead8f773e8ff252
Alexander Marchenko <maappdev@gmail.com>
2014-11-16 01:17:19 +0200

    fix route

--- app.js
+++ app.js
@@ -74,8 +74,9 @@ app.use(helpers(app));
 // 			console.log(err);
 // 		});
 // });
-app.use('/static', proxy('http://192.163.201.155:3131', {
+app.use('/static/*', proxy('http://192.163.201.155:3131', {
   forwardPath: function(req, res) {
+  	console.log(require('url').parse(req.url).path, require('url').parse(req.url));
     return require('url').parse(req.url).path;
   }
 }));
