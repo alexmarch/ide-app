bcbeb7bb0aad928e4e6d7b4082c3931660b8a634
Alexander Marchenko <maappdev@gmail.com>
2014-11-15 19:32:29 +0200

    fix proxy settings

--- api/services/proxyService.js
+++ api/services/proxyService.js
@@ -4,6 +4,7 @@ httpProxy = require('http-proxy');
 
 function ProxyService(opt){
 	opt = opt || {};
+	debug("Proxy options:",opt, "port:", opt.port || 8080);
 	this.proxy = httpProxy.createProxyServer(options).listen(opt.port || 8080);
 	this.proxy.on("error", this.error);
 };
