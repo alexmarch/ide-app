48020536720bcae328334fe9f37bd53d60b1c9f4
Alexander Marchenko <maappdev@gmail.com>
2014-11-15 19:22:18 +0200

    fix module services

--- api/services/proxyService.js
+++ api/services/proxyService.js
@@ -1,9 +1,9 @@
 var options = require('../../config/proxy.js'),
 debug = require('debug')('proxy'),
 httpProxy = require('http-proxy'),
-proxyConfig = require('./config/proxy.js'),
+proxyConfig = require('./config/proxy.js');
 
-module.export = function ProxyService(opt){
+module.exports = function ProxyService(opt){
 	this.proxy = httpProxy.createProxyServer(proxyConfig).listen(opt.port);
 	this.proxy.on("error", this.error);
 };
